267e1670b4a8dca8b9163112f8730f6969b6f1e9
Nelli Gorlova <nelli7035@gmail.com>
2024-10-25 04:06:38 +0600

    Created a function for adding posts

---
+++ src/components/Posts/Posts.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+const Posts = () => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default Posts;
--- src/containers/Add/Add.tsx
+++ src/containers/Add/Add.tsx
@@ -1,19 +1,48 @@
 import * as React from 'react';
+import { useState } from 'react';
+import { IPost } from '../../types';
 
 const Add = () => {
+  const [post, setPost] = useState<IPost>({
+    title: '',
+    description: '',
+  });
+
+  const [posts, setPosts] = useState<IPost[]>([]);
+
+  const addPost = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (post.title.length === 0 || post.description.length === 0) {
+      alert('Enter valid title and description');
+    } else {
+      const newPost = { id: String(new Date()), title: post.title, description: post.description };
+      setPosts([newPost, ...posts]);
+      setPost({ title: '', description: '' });
+      console.log(post);
+    }
+  };
+
   return (
     <div className="container mt-5">
       <div className="row border border-3 rounded">
         <h1 className="m-3">Add new post</h1>
         <div>
-          <form>
+          <form onSubmit={addPost}>
             <div className="m-3">
               <label className="form-label">Title</label>
-              <input type="title" className="form-control"/>
+              <input
+                value={post.title}
+                type="text"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPost({ ...post, title: e.target.value })}
+                className="form-control" />
             </div>
             <div className="m-3">
               <label className="form-label">Description</label>
-              <textarea type="Description" className="form-control"/>
+              <textarea
+                value={post.description}
+                onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => setPost({ ...post, description: e.target.value })}
+                className="form-control"></textarea>
             </div>
 
             <div className="m-3">
