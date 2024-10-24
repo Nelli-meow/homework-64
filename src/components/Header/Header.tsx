eedffa4c33cd4b473233ae2892f465c9524954be
Nelli Gorlova <nelli7035@gmail.com>
2024-10-24 21:10:26 +0600

    Created Header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <header className="bg-danger-subtle">
+      <nav className="navbar navbar-expand-lg">
+        <div className="container">
+          <NavLink className="text-black p-2 fs-4 text-decoration-none" to="/">Nelli-meow</NavLink>
+          <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <NavLink className="text-black p-2 text-decoration-none" to="/">Home</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="text-black p-2 text-decoration-none" to="/add">Add</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="text-black p-2 text-decoration-none" to="/about">About</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="text-black p-2 text-decoration-none" to="/contacts">Contacts</NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
